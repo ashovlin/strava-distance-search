f817d4e6d74527ba51033b9b02ad07737cd43603
Alex Shovlin <ashovlin@gmail.com>
2020-01-09 20:15:46 -0600

    Implemented distance filtering on client
    Added some half-baked error handling in view while investigating issue, need to finish

--- distance_search/urls.py
+++ distance_search/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', views.Home, name='home'),
-    path('activities/<int:distance>', views.activities_json)
+    path('activities/', views.activities_json)
 ]
--- distance_search/views.py
+++ distance_search/views.py
@@ -2,28 +2,42 @@ from django.views.generic import TemplateView
 from django.shortcuts import render
 from allauth.socialaccount.models import SocialToken
 import requests
+from datetime import datetime
 from django.http import JsonResponse
+from django.utils import timezone
 
 def Home(request):
     context = {}
-
     if request.user.is_authenticated:
-        access_token = SocialToken.objects.get(account__user=request.user, account__provider='strava')
-        context['activities'] = load_activities(access_token)
         return render(request, 'home.html', context)
     else:
         return render(request, 'index.html', context)
 
-def load_activities(access_token):
-    endpoint = 'https://www.strava.com/api/v3/athlete/activities'
-    headers = {"Authorization": "Bearer " + access_token.token}
-    response = requests.get(endpoint, headers=headers)
-    return response.json()
 
-def activities_json(request,distance):
-    print(distance)
+def activities_json(request):
     if request.user.is_authenticated:
         access_token = SocialToken.objects.get(account__user=request.user, account__provider='strava')
-        return JsonResponse(load_activities(access_token),safe=False)
+        return JsonResponse(load_activities(access_token), safe=False)
     else:
-        return JsonResponse("")
+        return JsonResponse("Not Authenticated")
+
+def load_activities(access_token):
+
+    if access_token.expires_at <= timezone.now():
+        print("TODO - need to refresh token")
+
+    endpoint = 'https://www.strava.com/api/v3/athlete/activities'
+    headers = {"Authorization": "Bearer " + access_token.token}
+    response = requests.get(endpoint, params={'per_page': 10}, headers=headers)
+
+    output = []
+    if response.status_code == 200:
+        for activity in response.json():
+            new_activity = {}
+            new_activity['name'] = activity['name']
+            new_activity['date'] = datetime.strptime(activity['start_date_local'].split('T')[0], "%Y-%M-%d").date()
+            new_activity['distance'] = round(activity['distance'] / 1609, 2)
+            output.append(new_activity)
+        return output
+    else:
+        print("TODO - ERROR FROM STRAVA")
