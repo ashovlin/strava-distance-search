badf3642101061aca710ae0cbd7d00e366b152c4
Alex Shovlin <ashovlin@gmail.com>
2020-01-11 13:06:44 -0600

    Implemented expired token refresh

--- distance_search/views.py
+++ distance_search/views.py
@@ -1,10 +1,9 @@
-from django.views.generic import TemplateView
-from django.shortcuts import render
-from allauth.socialaccount.models import SocialToken
-import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.http import JsonResponse
+from django.shortcuts import render
 from django.utils import timezone
+from allauth.socialaccount.models import SocialToken, SocialApp
+import requests
 
 def Home(request):
     context = {}
@@ -17,29 +16,53 @@ def Home(request):
 def activities_json(request):
     if request.user.is_authenticated:
         access_token = SocialToken.objects.get(account__user=request.user, account__provider='strava')
-        activities = load_activities(access_token)
+
         if access_token.expires_at <= timezone.now():
-            return JsonResponse({'error': "Token is Expired", 'activities': None}, safe=False)
-        
+            refresh_token(access_token)
+
+            if access_token.expires_at <= timezone.now():
+                return JsonResponse({'error': "Unable to refresh access token", 'activities': None}, safe=False)
+
+        activities = load_activities(access_token)
         return JsonResponse({'error': "", 'activities': activities}, safe=False)
-    else:
-        return JsonResponse({'error': "Not logged in", 'activities': None}, safe=False)
+
+    return JsonResponse({'error': "Unknown Error", 'activities': None}, safe=False)
 
 def load_activities(access_token):
 
     if access_token.expires_at <= timezone.now():
-        print("TODO - need to refresh token")
+        return
 
     endpoint = 'https://www.strava.com/api/v3/athlete/activities'
     headers = {"Authorization": "Bearer " + access_token.token}
-    response = requests.get(endpoint, params={'per_page': 10}, headers=headers)
+    response = requests.get(endpoint, params={'per_page': 100}, headers=headers)
 
     output = []
     if response.status_code == 200:
         for activity in response.json():
-            new_activity = {}
+            new_activity = {}   # Only send the fields we use
+            new_activity['id'] = activity['id']
             new_activity['name'] = activity['name']
-            new_activity['date'] = datetime.strptime(activity['start_date_local'].split('T')[0], "%Y-%M-%d").date()
+            new_activity['date'] = datetime.strptime(activity['start_date_local'].split('T')[0], "%Y-%m-%d").date()
             new_activity['distance'] = round(activity['distance'] / 1609, 2)
             output.append(new_activity)
     return output
+
+def refresh_token(access_token):
+    """Refreshes a user's Strava access token"""
+    strava = SocialApp.objects.get(provider='strava')
+
+    if not strava:
+        raise Exception("Unable to load strava secrets while refreshing token")
+
+    params = {'client_id': strava.client_id, 'client_secret': strava.secret, 'grant_type': 'refresh_token', 'refresh_token': access_token.token_secret}
+    response = requests.post("https://www.strava.com/oauth/token", data=params)
+
+    if response.status_code != 200:
+        Exception("Unexpected code while refreshing token: %d" % response.status_code)
+
+    response = response.json()
+    access_token.token = response['access_token']
+    access_token.expires_at = timezone.now() + timedelta(seconds=response['expires_in'])
+    access_token.token_secret = response['refresh_token']
+    access_token.save()
